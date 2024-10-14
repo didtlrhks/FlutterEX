6b36abab66a234cb03393270231df3902178bafa
sikwan <yysskk99999@naver.com>
2024-10-14 09:06:50 +0900

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(APP());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: Text("hello world"),
+    );
+  }
 }
